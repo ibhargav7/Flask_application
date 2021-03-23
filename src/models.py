d6b04af62729043dd7a6bec64ac7d63697a04008
ibhargav7 <bhargavinturi792@gmail.com>
2021-03-23 23:31:16 +0530

    add templates

--- src/models.py
+++ src/models.py
@@ -2,24 +2,12 @@ from json import loads
 from src import db
 import requests
 
-class city:
-    city = db.Column(db.Integer(), nullable=False, unique=True )
-    country = db.Column(db.String(length=30), nullable=False)
-class data:
-    def __init__(self,data):
-        self.time = data['dt']
-        self.city = data['name']
-        self.country= data['sys']['country']
-        self.main = data['weather'][0]['main']
-        self.descreption = data['weather'][0]['description']
-        self.icon = data['weather'][0]['icon']
-        self.temp = data['main']['temp']
-        self.feels_like = data['main']['feels_like']
-        self.temp_min = data['main']['temp_min']
-        self.temp_max = data['main']['temp_max']
-        self.pressure = data['main']['pressure']
-        self.humidity = data['main']['humidity']
-        self.sea_level = data['main']['sealevel']
+class City(db.Model):
+    city_name = db.Column(db.String(100), nullable=False, primary_key=True )
+
+class Citys(db.Model):
+    city_name = db.Column(db.String(100), nullable=False, primary_key=True )
+
 def weatherReport(city):
     api_key = "2a12d94d9078ebeebb34ccf5564f0cee"
     api = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
@@ -28,4 +16,7 @@ def weatherReport(city):
     data = response.json()
     return data
 
-    
+cities = City.query.all()
+
+
+
--- src/routes.py
+++ src/routes.py
@@ -1,29 +1,30 @@
 from src import app
 from src.forms import weatherForm
-from src.models import city,data
 from flask import redirect, url_for, flash, Flask, render_template, request
-from src.models import weatherReport
+from src.models import weatherReport, cities
 
-@app.route('/', methods=['GET','POST'])
-
-def weatherUpdate():
+@app.route('/', methods=['GET'])
+def weatherUpdate1():
+    print(cities)
     form = weatherForm()
     if form.validate_on_submit():
         return redirect(url_for("weatherUpdate1"))
+    return render_template('home.html',report=None, cities = cities, ureport= weatherReport)
 
-    return render_template('home.html', form=form)
-@app.route('/result', methods=['GET','POST'])
-def weatherUpdate1():
-    form = weatherForm()
-    city=request.form.get("city_name")
+@app.route('/result', methods=['POST'])
+def weatherUpdate():
+    city=request.form["city"]
     report = weatherReport(city)
-    return render_template('report.html', report=report, form=form)
-@app.route("/ajaxlivesearch", methods=["POST", "GET"])
+    print(report)
+    return render_template('home.html', report=report, cities = cities, ureport= weatherReport)
+
+"""@app.route("/ajaxlivesearch", methods=["POST", "GET"])
 def ajaxlivesearch():
     if request.method == 'POST':
         search_word = request.form['query']
         if search_word == '':
             pass
         else:
-            data = city.query.filter(city.city.like(search_word))
+            data = City.query.filter(city.city.like(search_word))
     return render_template('home.html', city=data)
+"""
